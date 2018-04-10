5d7833ff22015b38f88b3abf036bde239e92faee
Forrest Ross <forrestross1812@gmail.com>
2018-04-10 12:40:42 -0600

    Updated some more

--- Dungeon.java
+++ Dungeon.java
@@ -2,39 +2,79 @@ import java.util.*;
 
 public class Dungeon
 {
+   private static int itemXAxis;
+   private static int itemYAxis;
+   private static int xAxis;
+   private static int yAxis;
+   private static Rooms[][] roomArray = new Rooms[3][3];
+   
    
    public static void main (String[] args)
    {
       Random rand = new Random();
-      Rooms[][] roomArray = new Rooms[3][3];
+      
       Scanner input = new Scanner(System.in);
       ArrayList<Integer> roomGenerator = new ArrayList<Integer>();
-      int xAxis;
-      int yAxis;
       
       
       //CREATE ROOM GRID
       int roomInitializer = rand.nextInt(9);
       ArrayList<Integer> numUsed = new ArrayList<Integer>(); 
          
-         for(int i = 0; i < 3; ++i)
+      for(int i = 0; i < 3; ++i)
+      {
+         for(int j = 0; j < 3; ++j)
          {
-            for(int j = 0; j < 3; ++j)
+            if(roomInitializer == 1)
             {
-               if(roomInitializer == 1)
-               {
-                  xAxis = j;
-                  yAxis = i;
-                  
-               }
-               roomArray[i][j] = new Rooms(roomInitializer);
-               while (numUsed.contains(roomInitializer))
-               {
+               xAxis = j;
+               yAxis = i;
+                 
+            }
+            roomArray[i][j] = new Rooms(roomInitializer);
+            while (numUsed.contains(roomInitializer))
+            {             
                   roomInitializer = rand.nextInt(9);
-               }
             }
          }
-         
+      }
+      
+      
+      //PLACE ITEMS IN ROOMS
+      
+      ArrayList<Object> itemList = new ArrayList<Object>();
+      itemXAxis = rand.nextInt(3);
+      itemYAxis = rand.nextInt(3);
+      //KEY
+      itemList.add(roomArray[itemXAxis][itemYAxis]);
+      roomArray[itemXAxis][itemYAxis].hasItem("key");
+      
+      //BONE
+      while(itemList.contains(roomArray[itemXAxis][itemYAxis]))
+      {
+         itemXAxis = rand.nextInt(3);
+         itemYAxis = rand.nextInt(3);
+      }
+      itemList.add(roomArray[itemXAxis][itemYAxis]);
+      roomArray[itemXAxis][itemYAxis].hasItem("bone");
+      
+      //COMBINATION
+      while(itemList.contains(roomArray[itemXAxis][itemYAxis]))
+      {
+         itemXAxis = rand.nextInt(3);
+         itemYAxis = rand.nextInt(3);
+      }
+      itemList.add(roomArray[itemXAxis][itemYAxis]);
+      roomArray[itemXAxis][itemYAxis].hasItem("combination");
+      
+      //CABLE CUTTERS
+      while(itemList.contains(roomArray[itemXAxis][itemYAxis]))
+      {
+         itemXAxis = rand.nextInt(3);
+         itemYAxis = rand.nextInt(3);
+      }
+      itemList.add(roomArray[itemXAxis][itemYAxis]);
+      roomArray[itemXAxis][itemYAxis].hasItem("cable cutters");   
       
 
       //CREATE AN INSTANCE OF PLAYER
@@ -44,24 +84,47 @@ public class Dungeon
       
       //BEGIN GAME
       System.out.println("You wake up in a room that you don't recognize.\n The last thing you remember is going to bed at \nhome. As you stand up, a piece of paper falls to \nthe ground. You reach down and pick it up.");
-      System.out.print("\nWhat do you do next? ");
-      String option = string.toLowerCase(input.nextLine());
       
+      String option = "";
+      while (!option.equals("quit"))
+      {
+         System.out.print("\nWhat do you do next? ");
+         option = input.nextLine();
+         option = option.toLowerCase();
+         System.out.print(option);
+         options(option);
+      }
       
    }
    
-   private void options(String option)
+   private static void options(String option)
    {
-      //MOVE IN A DIRECTION
-      if (option == "go west" || option == "move west")
+      switch (option)
       {
-         if(xAxis != 0)
-         {
-            xAxis--;
-            roomArray[xAxis][yAxis].getDescription;
-         }
-      }
+         case "go west": 
+         case "move west":
+            if(xAxis != 0)
+            {
+               xAxis--;
+               roomArray[xAxis][yAxis].getDescription();
+            }
+            else
+            {
+               System.out.print(xAxis);
+               System.out.println("You cannot go that direction.");
+            }
+            break;
+         
+      
+
       
       //LOOK AROUND
+         case "look around": 
+         case "look":
+            System.out.println(roomArray[xAxis][yAxis].getDescription());
+      
+      
+      
+      }
    }
 }
--- Rooms.java
+++ Rooms.java
@@ -4,6 +4,9 @@ public class Rooms
    private static final String[] roomsArray = {"kitchen", "bedroom", "bathroom", "den", "laboratory", "dining room", "garage", "storage closet", "front entrance"};
    private static final String[] roomDescriptionArray = new String[9];
    private boolean hasKey = false;
+   private boolean hasBone = false;
+   private boolean hasCombination = false;
+   private boolean hasCableCutters = false;
    private String room;
    private String description;
    
@@ -25,5 +28,25 @@ public class Rooms
    public String getDescription()
    {
       return description;
+   }
+   
+   public void hasItem(String item)
+   {
+      if (item == "key")
+      {
+         hasKey = true;
+      }
+      else if (item == "bone")
+      {
+         hasBone = true;
+      }
+      else if (item == "combination")
+      {
+         hasCombination = true;
+      }
+      else if (item == "cable cutters")
+      {
+         hasCableCutters = true;
+      }
    }
 }
